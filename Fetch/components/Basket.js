513c400bc8615d3ee17e5d939e14dbddbcdce5c0
DESKTOP-VU5JPAT\Natella <gomza.natella@gmail.com>
2022-06-02 21:20:39 +0300

    with working fetch

--- Fetch/components/Basket.js
+++ Fetch/components/Basket.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { addToBasket } from './events';
 import { deleteFromBasket } from './events';
 import { connect } from 'react-redux';
-import isoFetch from 'isomorphic-fetch';
 
 import './Basket.css'
 
 class InitBasket extends React.PureComponent {
 
-    dataBaseName = 'GNR_Optik_Shop_GAME_DATA';
+    dataBaseName = 'GNR_React_Shop_GAME_DATA';
     dataBaseServerURL = "https://fe.it-academy.by/AjaxStringStorage2.php";
     updatePassword = null;
     data = [];
@@ -23,15 +22,14 @@ class InitBasket extends React.PureComponent {
 
     deleteProduct = (deleteProduct) => {
         this.deletedProduct = deleteProduct;
-        console.log(this.deletedProduct);
         this.updateBasket();
-        this.readStorage();
     }
 
     addProductToBasket = (newProduct) => {
         this.item = newProduct;
         this.updateBasket();
     }
+
     updateBasket() {
         this.updatePassword = Math.random();
 
@@ -43,7 +41,7 @@ class InitBasket extends React.PureComponent {
         fetch(this.dataBaseServerURL, { method: 'post', body: postRequestBeforeUpdate })
             .then(response => response.json())
             .then(data => this.lockGetReady(data))
-            .catch(error => { console.log(error); });
+            .catch(error => {alert("Ошибка сервера! Повторите попытку позднее!")});
     }
 
     lockGetReady(callresult) {
@@ -62,97 +60,28 @@ class InitBasket extends React.PureComponent {
                 this.data = this.data.filter(el => {
                     console.log(el);
                     return el.id !== this.deletedProduct
-                }
-                );
+                });
                 console.log(this.data);
             }
         }
-
+        console.log(this.data);
         let postRequestUpdate = new URLSearchParams();
         postRequestUpdate.append('f', 'UPDATE');
         postRequestUpdate.append('n', this.dataBaseName);
         postRequestUpdate.append('p', this.updatePassword);
-        postRequestUpdate.append('v', this.data);// вот тут нужно первый раз положить пустой массив
-
+        postRequestUpdate.append('v', JSON.stringify(this.data));// вот тут нужно первый раз положить пустой массив
 
         fetch(this.dataBaseServerURL, { method: 'post', body: postRequestUpdate })
             .then(response => response.json())
-            .then(data => this.saveResult(data))
-            .catch(error => { console.log(error); });
+            .then(data => this.readStorage())
+            .catch(error => alert("Ошибка сервера! Повторите попытку позднее!"));
 
         this.deletedProduct = null;
     }
 
-
-    // fetch(ajaxHandlerScript, { method: 'post', body: sp })
-    // .then( response => response.json() )
-    // .then( data => { lockGetReady; } )
-    // .catch( error => { console.error(error); } );
-
-    // updateBasket = async () => {
-    //     this.updatePassword = Math.random();
-
-    //     let postRequestBeforeUpdate = new URLSearchParams();
-    //     postRequestBeforeUpdate.append('f', 'LOCKGET');
-    //     postRequestBeforeUpdate.append('n', this.dataBaseName);
-    //     postRequestBeforeUpdate.append('p', this.updatePassword);
-
-
-    //     const response = await isoFetch(this.dataBaseServerURL, { method: 'post', body: postRequestBeforeUpdate });
-    //     if (!response.ok) {
-    //         this.fetchError("fetch error " + response.status);
-    //     }
-    //     else {
-    //         const data = await response.json();
-    //         console.log("fetch update");
-    //         this.lockGetReady(data);
-    //     }
-    // };
-
-    // lockGetReady = async (callresult) => {
-    //     console.log(callresult.result);
-    //     if (this.item && callresult.result) {
-    //         this.data = JSON.parse(callresult.result);
-    //         if (this.data) {
-    //             this.data.push(this.item);
-    //         }
-    //     }
-
-    //     if (this.deletedProduct && callresult.result) {
-    //         console.log(this.data);
-    //         this.data = JSON.parse(callresult.result);
-    //         if (this.data) {
-    //             this.data = this.data.filter(el => {
-    //                 console.log(el);
-    //                 return el.id !== this.deletedProduct
-    //             }
-    //             );
-    //             console.log(this.data);
-    //         }
-    //     }
-    //     console.log(this.data);
-    //     let postRequestUpdate = new URLSearchParams();
-    //     postRequestUpdate.append('f', 'UPDATE');
-    //     postRequestUpdate.append('n', this.dataBaseName);
-    //     postRequestUpdate.append('p', this.updatePassword);
-    //     postRequestUpdate.append('v', this.data);// вот тут нужно первый раз положить пустой массив
-
-
-    //     const updateResponse = await isoFetch(this.dataBaseServerURL, { method: 'post', body: postRequestUpdate });
-    //     console.log(updateResponse);
-    //     if (!updateResponse.ok) {
-    //         this.fetchError("fetch error " + response.status);
-    //     }
-    //     else {
-    //         const data = await updateResponse.json();
-    //         console.log(data);
-    //         this.saveResult(data);
-    //     }
-
-    //     this.deletedProduct = null;
-    // }
-
     readStorage() {
+        this.updatePassword = Math.random();
+
         let postRequestUpdate = new URLSearchParams();
         postRequestUpdate.append('f', 'READ');
         postRequestUpdate.append('n', this.dataBaseName);
@@ -160,14 +89,20 @@ class InitBasket extends React.PureComponent {
 
         fetch(this.dataBaseServerURL, { method: 'post', body: postRequestUpdate })
             .then(response => response.json())
-            .then(data => this.saveResult(data))
-            .catch(error => {console.log(error); });
+            .then(data => this.readData(data))
+            .catch(error => alert("Ошибка сервера! Повторите попытку позднее!"));
     }
 
-    saveResult(callresult) {
-        console.log(callresult.result)
-        let newProduct = JSON.parse(callresult.result);
-        this.props.dispatch({ type: "addToCart", payload: newProduct });
+    readData(data) {
+        console.log(data.result);
+        if (!data.result) {
+            this.data = [];
+            console.log(this.data);
+            this.updateBasket();
+        } else {
+            this.data = JSON.parse(data.result);
+            this.props.dispatch({ type: "addToCart", payload: this.data });
+        }
     }
 
     deleteFromCart = () => {
@@ -177,7 +112,6 @@ class InitBasket extends React.PureComponent {
     }
 
     render() {
-
         return (
             <div></div>
         )
